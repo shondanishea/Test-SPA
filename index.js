b90ea099cd3871ab166886ea4ea58b4ce72aaa94
Shondanishea Edwards <shondaxavier2@gmail.com>
2021-03-29 20:25:16 -0500

    update

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function addPicOnFormSubmit(st) {
         toppings: toppings
       };
       axios
-        .post(`http://localhost:4040/pizzas`, requestData)
+        .post(`https://shonda-pizza-testspa.herokuapp.com//pizzas`, requestData)
         .then(response => {
           state.Pizza.pizzas.push(response.data);
           router.navigate("/Pizza");
@@ -112,7 +112,7 @@ function fetchDataByView(st = state.Home) {
   switch (st.page) {
     case "Pizza":
       axios
-        .get(`http://localhost:4040/pizzas`)
+        .get(`https://shonda-pizza-testspa.herokuapp.com//pizzas`)
         .then(response => {
           state[st.page].pizzas = response.data;
           render(st);
