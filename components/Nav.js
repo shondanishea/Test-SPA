2115a5df149e40e8e485c1f9117f6212b2d97956
Shondanishea Edwards <shondaxavier2@gmail.com>
2021-03-29 19:21:27 -0500

    update

--- components/Nav.js
+++ components/Nav.js
@@ -2,14 +2,14 @@ export default links => `
 <nav>
   <i class="fas fa-bars"></i>
   <ul class="hidden--mobile nav-links">
-  ${links
-    .map(
-      link =>
-        `<li><a href="/${link.title !== "Home" ? link.title : ""}" title="${
-          link.title
-        }" data-navigo>${link.text}</a></li>`
-    )
-    .join("")}
+  ${links.reduce(
+    (html, link) =>
+      html +
+      `<li><a href="/${link.title !== "Home" ? link.title : ""}" title="${
+        link.title
+      }" data-navigo>${link.text}</a></li>`,
+    ``
+  )}
   </ul>
 </nav>
 `;
